6aa11521fb9dc58812af16cc1f85af65a05e8c05
Mathieu Briau <mathieu.briau@gmail.com>
2024-04-20 15:01:59 -0400

    retrait de vieux code

--- BackgroundService/Program.cs
+++ BackgroundService/Program.cs
@@ -17,7 +17,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//builder.Services.AddHostedService<Game>();
 builder.Services.AddSingleton<Game>();
 builder.Services.AddHostedService<Game>(p => p.GetService<Game>());
 
