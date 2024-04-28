f8f88d66c293ffd50e388e64f216d413ec8ab3a7
Mathieu Briau <mathieu.briau@gmail.com>
2024-04-28 10:57:06 -0400

    Ajout d'une page d'outils pour pouvoir appliquer les migrations

---
+++ BackgroundService/Controllers/ToolsController.cs
@@ -0,0 +1,35 @@
+﻿using BackgroundService.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackgroundService.Controllers
+{
+    public class ToolsController : Controller
+    {
+        private readonly BackgroundServiceContext _context;
+
+        public ToolsController(BackgroundServiceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Matches
+        public async Task<IActionResult> Index()
+        {
+            List<string> result = (await _context.Database.GetPendingMigrationsAsync()).ToList();
+            this.ViewData["pendingmigrations"] = result;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ApplyMigrations()
+        {
+            _context.Database.Migrate();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
